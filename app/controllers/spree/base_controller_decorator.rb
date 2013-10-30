0b70baf7225ed668cd0856bf209cb62c59c4190c
Filipe Moreira Guedes <fmg@markuptree.com>
2013-10-30 16:10:56 +0000

    Bugfix

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -7,7 +7,7 @@ Spree::BaseController.class_eval do
 			if (order.payment_state == "paid") or (order.payment_state == "credit_owed")
 				session[:order_id] = nil
 				order.line_items.destroy_all
-				flash[:notice] = t('spree.paypal_website_standard.payment_received')
+				flash[:notice] = t('spree.paypal_website_standard.payment_received') if order.payments.any? and order.payments.first.payment_method.type == "Spree::BillingIntegration::PaypalWebsiteStandard"
 			end
 		end
 	end
